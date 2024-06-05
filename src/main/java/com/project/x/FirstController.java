b8856e38125f9bceee3297dc1e3e217bd3343f04
samsonmeseret <samsonmeseretb@gmail.com>
2024-06-05 01:34:46 -0700

    using records

--- src/main/java/com/project/x/FirstController.java
+++ src/main/java/com/project/x/FirstController.java
@@ -19,10 +19,19 @@ public class FirstController {
         return "Request Accepted and message is :"+ message;
     };
 
-
+// using the Order object (Order Class)
     @PostMapping("/post-order")
     public String post(
             @RequestBody  Order order // converts every coming body(json, text...) in to string
+    ){
+        return "Request Accepted and message is :"+ order.toString();
+    };
+
+
+    // using the record(new Java feature) object (Order Class)
+    @PostMapping("/post-order-record")
+    public String post(
+            @RequestBody  OrderRecord order // converts every coming body(json, text...) in to string
     ){
         return "Request Accepted and message is :"+ order.toString();
     };
---
+++ src/main/java/com/project/x/OrderRecord.java
@@ -0,0 +1,8 @@
+package com.project.x;
+
+public record OrderRecord(
+        String customerName,
+        String productName,
+        int quantity
+) {
+}
